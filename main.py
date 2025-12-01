54b49f6d929648064bff4dcaa461c0bf103bd52d
mahavan <info@mahovsky.net>
2025-12-01 14:20:37 +0100

    add uppercase, lowercase word count

--- main.py
+++ main.py
@@ -40,17 +40,32 @@ print("Enter your username and password.")
 
 username = input("Username: ")
 password = input("Password: ")
-print()
+print("-" * 35)
 
 if users.get(username) == password:
     print(f"Welcome {username}!")
     print(f"We have {len(TEXTS)} texts to be analyzed.")
+
     text_choice = input(
         f"Enter a number between 1 and {len(TEXTS)} to select a text: ")
-    if not text_choice.isdigit() or not (1 <= int(text_choice) <= len(TEXTS)):
-        print("Invalid selection. Exiting the program.")
+    if not text_choice.isdigit():
+        print("You must enter a number! Exiting the program.")
         exit()
-    print(text_choice)
+    if not (1 <= int(text_choice) <= len(TEXTS)):
+        print("Invalid text selection. Exiting the program.")
+        exit()
+
+    text = TEXTS[int(text_choice) - 1]
+    words = text.split()
+    title_words = [word for word in words if word.istitle()]
+    uppercase_words = [word for word in words if word.isupper()]
+    lowercase_words = [word for word in words if word.islower()]
+    
+    print(f"There are {len(words)} words in the selected text.")
+    print(f"There are {len(title_words)} titlecase words.")
+    print(f"There are {len(uppercase_words)} uppercase words.")
+    print(f"There are {len(lowercase_words)} lowercase words.")
+    
 else:
     print("Unregistered user or incorrect password. Exiting the program.")
     exit()
